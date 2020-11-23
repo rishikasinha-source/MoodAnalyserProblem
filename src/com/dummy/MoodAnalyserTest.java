b38a45270e1217b365ad008a48c34e49647e50de
rishika7-hub <srishika970@gmail.com>
2020-11-23 21:01:09 +0530

    TC1.1_Repeated

--- src/com/dummy/MoodAnalyserTest.java
+++ src/com/dummy/MoodAnalyserTest.java
@@ -8,17 +8,10 @@ import org.junit.Test;
 public class MoodAnalyserTest {
 	MoodAnalyser analyser=new MoodAnalyser();
 
-//	@Test
-//	public void  givenSad_WhenAnalyseSad_thenShouldSadMood() {
-//		Assert.assertEquals("SAD",analyser.analyseMood("I am in Sad Mood"));
-//	 }
-		
-//	@Test
-	//public void givenAny_WhenAnalyseAny_thenShouldReturnHappyMood() {
-		//Assert.assertEquals("HAPPY",analyser.analyseMood("I am in any Mood"));
-		
-		
-		
-	}
+@Test
+public void givenSad_WhenAnalyseSad_thenShouldSadMood() {
+	Assert.assertEquals("SAD",analyser.analyseMood("I am in Sad Mood"));
+}
+}
 
 

b3528a6d67caa244cd424a8bdf3d15b7853e55d6
rishika7-hub <srishika970@gmail.com>
2020-11-23 18:32:13 +0530

    UC1

--- src/com/dummy/MoodAnalyser.java
+++ src/com/dummy/MoodAnalyser.java
@@ -1,5 +1,13 @@
 package com.dummy;
 
 public class MoodAnalyser {
-
+	public String analyseMood(String message) {
+		if(message.contains("Sad")) {
+		return message;
+		}
+		else {
+		return message;
+			}
+		}
+	
 }
